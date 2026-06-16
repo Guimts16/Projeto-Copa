68d1c3fefecc9cdd88f72842d82abd323c319069
Guilherme <134653860+Guimts16@users.noreply.github.com>
2026-06-15 23:10:43 -0300

    Delete src/app/pages directory

--- src/app/pages/tela-inicial-component/tela-inicial.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-tela-inicial',
-  standalone: false,
-  templateUrl: './tela-inicial.html',
-  styleUrl: './tela-inicial.css',
-})
-export class TelaInicial {}
